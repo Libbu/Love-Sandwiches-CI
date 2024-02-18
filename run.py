ecf8c3ffa98bef0c7222b3b9d3ae020689dfc74b
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-02-18 22:10:32 +0000

    added function to push data to spreadsheet

--- run.py
+++ run.py
@@ -35,9 +35,10 @@ def get_sales_data():
 
 def validate_data(values):
     """
-    inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     try:
         [int(value) for value in values]
@@ -50,8 +51,21 @@ def validate_data(values):
             return False
     
     return True
-    print(values)
+
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print('updating sales worksheet')
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales owkrsheet updated successfully\n")
+
 
 
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+
 
